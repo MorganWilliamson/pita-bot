6211f6e38eabf02d4dccdccb19e0b02451c20f88
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-04-22 12:43:00 -0500

    Quick Express set-up.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-// Discord.js ex:
-const Discord = require("discord.js");
-const client = new Discord.Client();
+require("dotenv").config();
+const express = require("express");
+const server = express();
+const port = process.env.PORT || 8000;
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+server.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-client.on("message", (msg) => {
-    if (msg.content === 'ping') {
-        msg.reply("Pong!");
-    }
+server.listen(port, () => {
+    console.log(`Test server listening at http://localhost:${port} . . .`);
 });
 
-client.login("token");
+///////////////////////////
+
+/// Discord.js ex:
+// const Discord = require("discord.js");
+// const client = new Discord.Client();
+
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}!`);
+// });
+
+// client.on("message", (msg) => {
+//     if (msg.content === 'ping') {
+//         msg.reply("Pong!");
+//     }
+// });
+
+// client.login("token");
