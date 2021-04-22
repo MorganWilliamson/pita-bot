1fedaecb06a6a6a34a23919043a5429e1fbdb8cd
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-04-22 18:39:30 -0500

    Playing with messages, working so far!

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 const Discord = require("discord.js");
+const { prefix } = require("./config.json");
 const client = new Discord.Client();
-
 
 client.once("ready", () => {
     console.log("Ready!");
 });
 
+// Message handling: 
 client.on("message", (msg) => {
-    if (msg.content === '!ping') {
+    if (msg.content.startsWith(`${prefix}ping`)) {
         msg.channel.send("Pong!");
+    } else if (msg.content.includes("beep")) {
+        msg.channel.send("Boop!");
     }
 });
 
