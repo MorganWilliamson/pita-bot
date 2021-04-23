6ade63a695e3643db05a5025d826a572fc9e111d
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-04-23 16:44:04 -0500

    Updated commands, added avatar command.

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const { prefix } = require("./config.json");
 const client = new Discord.Client();
 
 client.once("ready", () => {
-    console.log("Ready!");
+    console.log("Pita is ready!");
 });
 
 // Message handling: 
 client.on("message", (msg) => {
+    /* 1). If the message doesn't start with the prefix or if the message is
+            written by a bot, then ignore the message. (DRY) */
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    /* 2). Create an `args` variable that will slice off the prefix entirely, 
+        remove the leftover whitespaces, then split it into an array by spaces. 
+       
+       3). Create a `command` variable (by calling `args.shift`), which will take
+       the first element in the array and return it while ALSO removing it from the
+       original array (so that you don't have a command name string inside the args
+       array.)
+    */
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
     if (msg.content.startsWith(`${prefix}ping`)) {
         msg.channel.send("Pong!");
     } else if (msg.content.includes("beep")) {
@@ -23,6 +38,25 @@ client.on("message", (msg) => {
             `); 
     } else if (msg.content.startsWith(`${prefix}region`)) {
         msg.channel.send(`Current Region: ${msg.guild.region}`)
+    } else if (command === 'args-info') {
+        if (!args.length) {
+            return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
+    }   else if (args[0] === 'foo') {
+        return msg.channel.send('bar');
+    }
+        msg.channel.send(`First argument: ${args[0]}`);
+        msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    } else if (command === "avatar") {
+        // Returning a link to a user's avatar, no image embedding? 
+        if (!msg.mentions.users.size) {
+            return msg.channel.send(`Your Avatar: <${msg.author.displayAvatarURL({ format: "png", dynamic: true})}>`);
+        }
+        
+        const avatarList = msg.mentions.users.map((user) => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true})}>`;
+        });
+
+        msg.channel.send(avatarList);
     }
 });
 
