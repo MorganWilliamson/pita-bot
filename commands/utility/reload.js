27247fe2c8b7540290c79151755189e385c9e317
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-05-10 17:17:32 -0500

    Reload command built, tested and working.

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -13,5 +13,20 @@ module.exports = {
         if (!command) {
             return msg.channel.send(`There's no command with name or alias \`${commandName}\`, ${msg.author}!`)
         }
+
+        const commandFolders = fs.readdirSync("./commands");
+        const folderName = commandFolders.find((folder) => 
+            fs.readdirSync(`./commands/${folder}`).includes(`${command.name}.js`));
+        
+        delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
+
+        try {
+            const newCommand = require(`../${folderName}/${command.name}.js`);
+            msg.client.commands.set(newCommand.name, newCommand);
+            msg.channel.send(`Command \`${newCommand.name}\` was successfully reloaded!`);
+        } catch (err) {
+            console.error(err);
+            msg.channel.send(`There was an error while reloading a command, \`${command.name}\`:\n\`${error.message}\``);
+        }
     },
 };
